2da62f9c1beba1d46b4a2f7a2738bdd9eb116991
Mahalakshmi <srinukarri918@gmail.com>
2026-07-21 22:53:42 +0530

    Create ifelse.c

---
+++ ifelse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    
+    printf("Enter 2 numbers: ");
+    scanf("%d %d", &a, &b);
+    
+    if(a > b) {
+        printf("%d is Biggest\n", a);
+    } else {
+        printf("%d is Biggest\n", b);
+    }
+    
+    return 0;
+}
